b7b5b98ccd733b55e139a842560034f372dadd4f
Guilhermecheng <guilherme.cheng@gmail.com>
2023-06-16 00:35:46 -0300

    style: cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -12,6 +12,7 @@ import { HiOutlineArrowCircleLeft } from "react-icons/hi";
 
 const CartSection = styled.section`
     margin-top: 24px;
+    padding: 0 16px;
 `;
 
 const BackButton = styled.button`
@@ -35,9 +36,18 @@ const BackButton = styled.button`
 
 const CartContent = styled.div`
     width:100%;
-    display: grid;
-    grid-template-columns: 2fr 1fr;
-    gap: 32px;
+    display: flex;
+    flex-direction: column;
+
+    margin-top: 40px;
+
+    @media (min-width:897px) {
+        display: grid;
+        grid-template-columns: 2fr 1fr;
+        gap: 32px;
+        margin-top: 0;
+
+    }
 `;
 
 const LeftSection = styled.div`
@@ -65,11 +75,86 @@ const RightSection = styled.div`
     color: var(--color-gray-800);
     background-color: white;
     padding: 16px 24px;
+    height: 700px;
+    border-radius: 8px;
+
+    position: relative;
 
     h1 {
         font-weight: 500;
         font-size: 24px;
         text-transform: uppercase;
+        margin-bottom: 30px;
+    }
+
+    div {
+        display: flex;
+        flex-direction: column;
+
+        span {
+            display: flex;
+            justify-content: space-between;
+            margin-bottom: 12px;
+
+            h3 {
+                font-style: normal;
+                font-weight: 400;
+                font-size: 16px;
+                line-height: 150%;
+            }
+
+            span {
+               margin: 0;
+            }
+        }
+    }
+
+    button {
+        margin-top: 40px;
+        width: 100%;
+        background-color: #51B853;
+        padding: 14px 0;
+        color: white;
+        border: none;
+        font-weight: 500;
+        font-size: 16px;
+        text-transform: uppercase;
+        cursor: pointer;
+        border-radius: 4px;
+
+        &:hover {
+            background-color: #66d068;
+        }
+    }
+
+    ul {
+        position: absolute;
+        bottom: 24px;
+
+        li {
+            list-style: none;
+            text-transform: uppercase;
+        }
+    }
+`;
+
+const TotalPrice = styled.span`
+    margin-top: 12px;
+    padding-top: 8px;
+    border-top: 1px solid #DCE2E6;
+    
+    h2 {
+        font-style: normal;
+        font-weight: 600;
+        font-size: 16px;
+        line-height: 150%;
+    }
+
+    span {
+        margin: 0;
+        font-weight: 600;
+        font-size: 16px;
+        border-top: none;
     }
 `;
 
@@ -107,6 +192,32 @@ export default function Cart() {
 
                         <RightSection>
                             <h1>Resumo do pedido</h1>
+
+                            <div>
+                                <span>
+                                    <h3>Subtotal dos produtos</h3>
+                                    <span>R$40,00</span>
+                                </span>
+
+                                <span>
+                                    <h3>Entrega</h3>
+                                    <span>R$40,00</span>
+                                </span>
+
+                                <TotalPrice>
+                                    <h2>Total</h2>
+                                    <span>R$40,00</span>
+                                </TotalPrice>
+                            </div>
+
+                            <button>FINALIZAR COMPRA</button>
+
+                            <ul>
+                                <li>Ajuda</li>
+                                <li>Reembolsos</li>
+                                <li>Entrega e frete</li>
+                                <li>Trocas e devoluções</li>
+                            </ul>
 
                         </RightSection>
                     </CartContent>
--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -2,6 +2,8 @@
 
 import { styled } from "styled-components"
 import { BsTrash3 } from "react-icons/bs";
+import { useContext, useEffect } from "react";
+import { GlobalContext } from "@/contexts/GlobalContext";
 
 const CartItemLI = styled.li`
     display: flex;
@@ -26,6 +28,24 @@ const CartItemLI = styled.li`
     div {
         padding: 16px 20px 16px 30px;
         position: relative;
+
+        h1 {
+            font-size: 20px;
+            font-style: normal;
+            font-weight: 300;
+            text-transform: none;
+            line-height: 150%;
+            letter-spacing: 1px;
+            margin-bottom: 12px;
+        }
+
+        p {
+            height: 72px;
+            overflow: hidden;
+            font-weight: 400;
+            font-size: 12px;
+            line-height: 150%;
+        }
     }
 `;
 
@@ -34,11 +54,19 @@ const QtyAndPrice = styled.div`
     bottom: 0; 
     right: 0;
     padding: 0 !important;
-    margin-top: 60px;
+    margin-top: 30px;
     display: flex;
 
     width: 100%;
     justify-content: space-between;
+
+    select {
+        background-color: #F3F5F6;
+        border: 1px solid #A8A8B3;
+        color: #737380;
+        border-radius: 8px;
+        padding: 8px 12px;
+    }
 
     span {
         font-weight: bold;
@@ -58,30 +86,51 @@ interface CartItemProps {
 
 
 export function CartItem({ cartProduct }: CartItemProps) {
-    const { id, name, description, price_in_cents, quantity, image_url } = cartProduct;
-    let price_in_brazilian_reais = (price_in_cents / 100).toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+    const { cart, setCart } = useContext(GlobalContext);
+    const valuesArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+
+    const { id, name, description, price_in_cents, quantity, image_url } = cartProduct;
+    let price_in_brazilian_reais = ((price_in_cents * quantity) / 100).toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+
+    function removeItemFromCart(itemId: string) {
+        let  cartItem = cart.findIndex(o => o.id === itemId);
+        cart.splice(cartItem, 1);
+        console.log(cart);
+        setCart([...cart]);
+    }
+
+    function handleChange(e: React.ChangeEvent<HTMLSelectElement>) {
+        e.preventDefault();
+        const value = e.target.value;
+        let  cartItem = cart.findIndex(o => o.id === id);
+        cart[cartItem].quantity = parseInt(value);
+        console.log(cart)
+        setCart([...cart]);
+    }
 
     return (
         <CartItemLI>
             <img src={ image_url } alt={name} />
 
             <div>
-                <BsTrash3 size={20} />
+                <BsTrash3 size={20} onClick={() => {removeItemFromCart(id)}} />
                 <h1>{name}</h1>
                 <p>{description}</p>
-            <QtyAndPrice>
-                    <select name="" id="">
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                    </select>
 
-                    <span>
-                        { price_in_brazilian_reais }
-                    </span>
-            </QtyAndPrice>
+                <QtyAndPrice>
+                        <select name={`${name} quantity`} id={`${id}-quantity`} value={quantity} onChange={handleChange}>
+                            { valuesArr.map((val) => {
+                                return (
+                                    <option key={val} value={val}>{val}</option>
+                                )
+                            })}
+                        </select>
+
+                        <span>
+                            { price_in_brazilian_reais }
+                        </span>
+                </QtyAndPrice>
             </div>
 
 
